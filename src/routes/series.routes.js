8a78ad4d2df289da5c85aa1a8bf3c60a3dc24546
Dinan <dinandilmith58@gmail.com>
2026-07-04 00:36:32 +0530

    feat: wired validators to routes

--- src/routes/series.routes.js
+++ src/routes/series.routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const seriesController = require("../controllers/series.controller");
+const validate = require("../middleware/validate.middleware");
+const {
+  searchValidator,
+  tmdbIdParam,
+} = require("../validators/series.validator");
 
-router.get("/search", seriesController.search);
-router.post('/:tmdbId/import', seriesController.importSeries);
+router.get("/search", searchValidator, validate, seriesController.search);
+router.post(
+  "/:tmdbId/import",
+  tmdbIdParam,
+  validate,
+  seriesController.importSeries,
+);
 
 module.exports = router;
--- src/routes/userEpisodes.routes.js
+++ src/routes/userEpisodes.routes.js
@@ -2,10 +2,22 @@ const express = require("express");
 const router = express.Router();
 const { requireAuth } = require("../middleware/auth.middleware");
 const userEpisodesController = require("../controllers/userEpisodes.controller");
+const validate = require("../middleware/validate.middleware");
+const { episodeIdParam } = require("../validators/userEpisodes.validator");
 
 router.use(requireAuth);
 
-router.post("/:episodeId", userEpisodesController.markWatched);
-router.delete("/:episodeId", userEpisodesController.unmarkWatched);
+router.post(
+  "/:episodeId",
+  episodeIdParam,
+  validate,
+  userEpisodesController.markWatched,
+);
+router.delete(
+  "/:episodeId",
+  episodeIdParam,
+  validate,
+  userEpisodesController.unmarkWatched,
+);
 
 module.exports = router;
