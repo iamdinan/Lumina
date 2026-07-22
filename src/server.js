4391b613c4a20de6453fdd366beeda8c5133dcbb
Dinan <dinandilmith58@gmail.com>
2026-07-22 14:03:25 +0530

    fix: update allowedOrigins to use single value from environment variable

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const userSeriesRoutes = require("./routes/userSeries.routes");
 const userEpisodesRoutes = require("./routes/userEpisodes.routes");
 const errorHandler = require("./middleware/error.middleware");
 const helmet = require("helmet");
-const allowedOrigins = process.env.FRONTEND_URL.split(",");
+const allowedOrigins = process.env.FRONTEND_URL;
 
 const app = express();
 app.use(
